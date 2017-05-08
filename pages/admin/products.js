97669460f2528e4e0105c954fb0d30e13395cce2
Guillermo Lopez <guillermolopez2529@gmail.com>
2017-05-07 23:32:46 -0400

    working with admin views

---
+++ pages/admin/products.js
@@ -0,0 +1,56 @@
+import Footer from '../../src/shared/components/Footer';
+import Header from '../../src/shared/components/Header';
+import Main from '../../src/shared/components/Main';
+import SecondaryHeader from '../../src/shared/components/SecondaryHeader';
+import Title from '../../src/shared/components/Title';
+import ProductBox from '../../src/product/components/ProductBox';
+import FilterForm from '../../src/product/components/FilterForm';
+import OrderBox from '../../src/order/components/OrderBox';
+
+const Products = props => (
+  <div className="index">
+    <Header>
+      <p>Header text</p>
+    </Header>
+    <Main>
+      <section className="mainLeft">
+        <SecondaryHeader>
+          <Title>Product List</Title>
+          <FilterForm />
+        </SecondaryHeader>
+      </section>
+      <section className="mainRight">
+      </section>
+    </Main>
+    <Footer />
+
+
+    <style>{`
+      body,html,h1,h2,h3,h4,h5,h6,ul,li,p {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+      }
+
+      .index {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+      }
+
+      .mainLeft {
+        display: flex;
+        flex: 1;
+        flex-direction: column;
+        padding: 1em;
+      }
+
+      .mainRight {
+        padding: 1em;
+        width: 255px;
+      }
+    `}</style>
+  </div>
+);
+
+export default Products;
